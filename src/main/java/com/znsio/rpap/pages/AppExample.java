a263f51a4d69c6915575f6b58681dbdcc967c640
raghavgarg1996 <raghav.gargril291900@gmail.com>
2023-09-21 13:15:44 +0530

    Adding Android test

---
+++ src/main/java/com/znsio/rpap/pages/AppExample.java
@@ -0,0 +1,76 @@
+package com.znsio.rpap.pages;
+
+import io.appium.java_client.AppiumDriver;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AppExample extends BasePage {
+
+    public static final By ByCalculatorScreenXpath = By.xpath("//android.widget.EditText");
+    private static final By ByCheckForUpdateId = By.id("android:id/button1");
+    public static final By ByWelcomeMsgId = By.id("com.android2.calculator3:id/cling_dismiss");
+    private static final Logger LOGGER = Logger.getLogger(AppExample.class.getName());
+    private static final Map<String, String> OPERATION_MAP = new HashMap<>();
+
+    static {
+        OPERATION_MAP.put("plus", "plus");
+        OPERATION_MAP.put("subtract", "minus");
+        OPERATION_MAP.put("multiply", "times");
+        OPERATION_MAP.put("divide", "divide");
+        OPERATION_MAP.put("equals", "equal");
+    }
+
+    public AppExample(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void handlePopupIfPresent() {
+        boolean isUpgradeAppNotificationElement = isElementPresent(
+                ByCheckForUpdateId);
+        if (isUpgradeAppNotificationElement) {
+            driver.findElement(ByCheckForUpdateId).click();
+            waitFor(1);
+        }
+        boolean isClingElementPresent = isElementPresent(
+                ByWelcomeMsgId);
+        if (isClingElementPresent) {
+            driver.findElement(ByWelcomeMsgId).click();
+            waitFor(1);
+        }
+    }
+
+    public boolean isElementPresent(By locator) {
+        return !driver.findElements(locator).isEmpty();
+    }
+
+    public synchronized static void waitFor(int seconds) {
+        LOGGER.info("Wait for " + seconds + " seconds");
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void chooseNumber(String number) {
+        driver.findElement(By.id("digit" + number)).click();
+    }
+
+    public void chooseOperation(String operation) {
+        String mappedOperation = OPERATION_MAP.get(operation.toLowerCase());
+
+        if (mappedOperation == null) {
+            throw new RuntimeException("Operation " + operation + " is not supported");
+        }
+        driver.findElement(By.id(mappedOperation)).click();
+    }
+
+    public String getCalculatorDisplay() {
+        return driver.findElement(ByCalculatorScreenXpath).getText().trim();
+    }
+}
---
+++ src/test/java/com/znsio/rpap/AppExampleTest.java
@@ -0,0 +1,78 @@
+package com.znsio.rpap;
+
+import com.epam.reportportal.annotations.Step;
+import com.znsio.rpap.pages.AppExample;
+import com.znsio.rpap.utils.JsonDataManager;
+import com.znsio.rpap.utils.JsonDataProvider;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+import static com.znsio.rpi.utils.ScreenShotManager.captureScreenShot;
+
+public class AppExampleTest extends BaseTest {
+
+    private AppExample AppPage;
+
+    private static final Logger LOGGER = Logger.getLogger(AppExampleTest.class.getName());
+
+    @Test(dataProvider = "getFromJson", priority = -1, description = "Perform operations on two no. on calculator",
+            groups = {"visual"})
+    public void calculatorTest(String input1, String operator1, String input2, String operator2, String expectedMsg) {
+
+        AppPage = page.getClassInstance(AppExample.class);
+        captureScreenShot(webDriver, "Validating Calculator screen");
+        startCalculator();
+        selectNumber(input1);
+        pressOperator(operator1);
+        selectNumber(input2);
+        pressOperator(operator2);
+        verifyResult(expectedMsg);
+    }
+
+    @Step
+    private void startCalculator() {
+        log("Handling calculator popUps");
+        AppPage.handlePopupIfPresent();
+        captureScreenShot(webDriver, "Pop Ups handled on calculator");
+    }
+
+    @Step
+    private void selectNumber(String number) {
+        LOGGER.info("Select No." + number);
+        AppPage.chooseNumber(number);
+        captureScreenShot(webDriver, "Number selected" + number);
+
+    }
+
+    @Step
+    private void pressOperator(String operation) {
+        LOGGER.info("Select Calculator Operation " + operation);
+        AppPage.chooseOperation(operation);
+        captureScreenShot(webDriver, "Operator selected " + operation);
+
+    }
+
+    @Step
+    private void verifyResult(String expected) {
+        LOGGER.info("Validating calculator output ");
+        LOGGER.info("Expected output " + expected);
+        String actualOutput = AppPage.getCalculatorDisplay();
+        LOGGER.info("Actual output " + actualOutput);
+        Assert.assertEquals(expected, actualOutput);
+        captureScreenShot(webDriver, "Final result captured");
+
+    }
+
+
+    @DataProvider(name = "getFromJson")
+    public Object[][] getFromJson(Method mtd) throws Exception {
+        JsonDataManager jdm = JsonDataProvider.loadData();
+        Object[][] dataMap = jdm.getData(getClass(), mtd.getName());
+        return dataMap;
+    }
+}
