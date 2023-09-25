fad48ba4058372d777ebad9212ab528c8db2bf31
raghavgarg1996 <raghav.gargril291900@gmail.com>
2023-09-25 11:28:26 +0530

    Updating logs and messages

--- src/test/java/com/znsio/rpap/AppExampleTest.java
+++ src/test/java/com/znsio/rpap/AppExampleTest.java
@@ -1,13 +1,13 @@
 package com.znsio.rpap;
 
+import com.applitools.eyes.appium.Target;
 import com.epam.reportportal.annotations.Step;
 import com.znsio.rpap.pages.AppExample;
 import com.znsio.rpap.utils.JsonDataManager;
 import com.znsio.rpap.utils.JsonDataProvider;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import java.lang.reflect.Method;
 import static com.znsio.rpi.utils.ScreenShotManager.captureScreenShot;
 
@@ -21,8 +21,7 @@ public class AppExampleTest extends AppBaseTest {
             groups = {"visual"})
     public void AdditionTest(String input1, String operator1, String input2, String operator2, String expectedMsg) {
         AppPage = page.getClassInstanceApp(AppExample.class);
-        captureScreenShot(appDriver, "Validating Calculator screen");
-        startCalculator();
+        handleCalculatorPopUps();
         performOperation(input1, operator1, input2, operator2);
         verifyResult(expectedMsg);
     }
@@ -30,45 +29,41 @@ public class AppExampleTest extends AppBaseTest {
     @Test(dataProvider = "getFromJson", priority = -1, description = "Perform subtraction of two no. on calculator",
             groups = {"visual"})
     public void SubtractionTest(String input1, String operator1, String input2, String operator2, String expectedMsg) {
-        captureScreenShot(appDriver, "Validating Calculator screen");
-        startCalculator();
+        handleCalculatorPopUps();
         performOperation(input1, operator1, input2, operator2);
         verifyResult(expectedMsg);
     }
 
+    //   @BeforeMethod
     @Step
-    private void startCalculator() {
+    private void handleCalculatorPopUps() {
         log("Handling calculator popUps");
         AppPage.handlePopupIfPresent();
-        captureScreenShot(appDriver, "Pop Ups handled on calculator");
-        //     eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
     }
 
     @Step
     private void verifyResult(String expected) {
-        LOGGER.info("Validating calculator output ");
         LOGGER.info("Expected output " + expected);
         String actualOutput = AppPage.getCalculatorDisplay();
         LOGGER.info("Actual output " + actualOutput);
         Assert.assertEquals(expected, actualOutput);
         captureScreenShot(appDriver, "Final result captured");
-        //     eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
+        eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
     }
 
     @Step
     public void performOperation(String input1, String operator1, String input2, String operator2) {
-        LOGGER.info("Select No." + input1);
         AppPage.chooseNumber(input1);
-        captureScreenShot(appDriver, "Number selected " + input1);
-        LOGGER.info("Select Calculator Operation " + operator1);
+        log("Selected No." + input1);
         AppPage.chooseOperation(operator1);
-        captureScreenShot(appDriver, "Operator selected " + operator1);
-        LOGGER.info("Select No." + input2);
+        log("Select Calculator Operation " + operator1);
         AppPage.chooseNumber(input2);
-        captureScreenShot(appDriver, "Number selected " + input2);
+        log("Select No." + input2);
+        captureScreenShot(appDriver, "Data entered on Calculator");
+        eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
         LOGGER.info("Select Calculator Operation " + operator2);
+        log("Select Calculator Operation " + operator2);
         AppPage.chooseOperation(operator2);
-        captureScreenShot(appDriver, "Operator selected " + operator2);
     }
 
 
