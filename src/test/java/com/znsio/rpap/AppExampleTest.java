60d8b342b8345d6495469ee5981eb5f8aa914c1a
raghavgarg1996 <raghav.gargril291900@gmail.com>
2023-09-26 16:05:02 +0530

    Creating appStep class

--- src/test/java/com/znsio/rpap/AppExampleTest.java
+++ src/test/java/com/znsio/rpap/AppExampleTest.java
@@ -3,77 +3,66 @@ package com.znsio.rpap;
 import com.applitools.eyes.appium.Target;
 import com.epam.reportportal.annotations.Step;
 import com.znsio.rpap.pages.AppExample;
+import com.znsio.rpap.steps.AppSteps;
 import com.znsio.rpap.utils.JsonDataManager;
 import com.znsio.rpap.utils.JsonDataProvider;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 import java.lang.reflect.Method;
+
 import static com.znsio.reportportal.integration.utils.ReportPortalLogger.captureAndAttachScreenshot;
 
 public class AppExampleTest extends AppBaseTest {
 
-    private AppExample AppPage;
+    private AppExample appPage;
+    private AppSteps appSteps;
     private static final Logger LOGGER = Logger.getLogger(AppExampleTest.class.getName());
+
+    @BeforeMethod
+    private void handleCalculatorPopUps() {
+        log("Inside @BeforeMethod of " + AppExampleTest.class.getSimpleName());
+        appPage = page.getClassInstanceApp(AppExample.class);
+        appPage.handlePopupIfPresent();
+        log("Handling calculator popUps");
+        appSteps = new AppSteps(appDriver, appPage, eyesOnWeb);
+    }
 
     @Test(dataProvider = "getFromJson", priority = -3, description = "Perform addition of two no. on calculator",
             groups = {"visual"})
     public void AdditionTest(String input1, String operator1, String input2, String operator2, String expectedMsg) {
-        performOperation(input1, operator1, input2, operator2);
-        verifyResult(expectedMsg);
+        appSteps.performOperation(input1, operator1, input2, operator2);
+        appSteps.verifyResult(expectedMsg);
+        //   performOperation(input1, operator1, input2, operator2);
+        //   verifyResult(expectedMsg);
     }
 
     @Test(dataProvider = "getFromJson", priority = -2, description = "Perform subtraction of two no. on calculator",
             groups = {"visual"})
     public void SubtractionTest(String input1, String operator1, String input2, String operator2, String expectedMsg) {
-        performOperation(input1, operator1, input2, operator2);
-        verifyResult(expectedMsg);
+        appSteps.performOperation(input1, operator1, input2, operator2);
+        appSteps.verifyResult(expectedMsg);
+        //   performOperation(input1, operator1, input2, operator2);
+        //   verifyResult(expectedMsg);
     }
 
     @Test(dataProvider = "getFromJson", priority = -1, description = "Perform multiplication of two no. on calculator",
             groups = {"visual"})
     public void MultiplicationTest(String input1, String operator1, String input2, String operator2, String expectedMsg) {
-        performOperation(input1, operator1, input2, operator2);
-        verifyResult(expectedMsg);
+        appSteps.performOperation(input1, operator1, input2, operator2);
+        appSteps.verifyResult(expectedMsg);
+        //   performOperation(input1, operator1, input2, operator2);
+        //   verifyResult(expectedMsg);
     }
 
     @Test(dataProvider = "getFromJson", description = "Perform divison of two no. on calculator",
             groups = {"visual"})
     public void DivisonTest(String input1, String operator1, String input2, String operator2, String expectedMsg) {
-        performOperation(input1, operator1, input2, operator2);
-        verifyResult(expectedMsg);
-    }
-
-    @BeforeMethod
-    private void handleCalculatorPopUps() {
-        AppPage = page.getClassInstanceApp(AppExample.class);
-        AppPage.handlePopupIfPresent();
-        log("Handling calculator popUps");
-    }
-
-    @Step
-    private void verifyResult(String expected) {
-        LOGGER.info("Expected output " + expected);
-        String actualOutput = AppPage.getCalculatorDisplay();
-        LOGGER.info("Actual output " + actualOutput);
-        Assert.assertEquals(expected, actualOutput);
-        captureAndAttachScreenshot(appDriver, "Final result captured");
-        eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
-    }
-
-    @Step
-    public void performOperation(String input1, String operator1, String input2, String operator2) {
-        AppPage.chooseNumber(input1);
-        log("Selected No." + input1);
-        AppPage.chooseOperation(operator1);
-        log("Select Calculator Operation " + operator1);
-        AppPage.chooseNumber(input2);
-        log("Select No." + input2);
-        captureAndAttachScreenshot(appDriver, "Data entered on Calculator");
-        eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
-        LOGGER.info("Select Calculator Operation " + operator2);
-        log("Select Calculator Operation " + operator2);
-        AppPage.chooseOperation(operator2);
+        appSteps.performOperation(input1, operator1, input2, operator2);
+        appSteps.verifyResult(expectedMsg);
+        //   performOperation(input1, operator1, input2, operator2);
+        //   verifyResult(expectedMsg);
     }
 
 
@@ -83,4 +72,33 @@ public class AppExampleTest extends AppBaseTest {
         Object[][] dataMap = jdm.getData(getClass(), mtd.getName());
         return dataMap;
     }
+
+/*
+    @Step
+    public void performOperation(String input1, String operator1, String input2, String operator2) {
+        appPage.chooseNumber(input1);
+        log("Selected No." + input1);
+        appPage.chooseOperation(operator1);
+        log("Select Calculator Operation " + operator1);
+        appPage.chooseNumber(input2);
+        log("Select No." + input2);
+        captureAndAttachScreenshot(appDriver, "Data entered on Calculator");
+        eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
+        //     LOGGER.info("Select Calculator Operation " + operator2);
+        log("Select Calculator Operation " + operator2);
+        appPage.chooseOperation(operator2);
+    }
+
+    @Step
+    public void verifyResult(String expected) {
+        LOGGER.info("Expected output " + expected);
+        String actualOutput = appPage.getCalculatorDisplay();
+        LOGGER.info("Actual output " + actualOutput);
+        Assert.assertEquals(expected, actualOutput);
+        captureAndAttachScreenshot(appDriver, "Final result captured");
+        eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
+    }
+*/
+
+
 }
---
+++ src/test/java/com/znsio/rpap/steps/AppSteps.java
@@ -0,0 +1,56 @@
+package com.znsio.rpap.steps;
+
+import com.applitools.eyes.appium.Target;
+import com.applitools.eyes.selenium.Eyes;
+import com.epam.reportportal.annotations.Step;
+import com.znsio.rpap.pages.AppExample;
+import io.appium.java_client.AppiumDriver;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+
+import static com.znsio.reportportal.integration.utils.ReportPortalLogger.captureAndAttachScreenshot;
+import static com.znsio.reportportal.integration.utils.ReportPortalLogger.logInfoMessage;
+
+public class AppSteps {
+
+    private static final Logger LOGGER = Logger.getLogger(AppSteps.class.getName());
+    private AppiumDriver appDriver;
+    private AppExample appPage;
+    private Eyes eyesOnApp;
+
+    public AppSteps(AppiumDriver appDriver, AppExample appPage, Eyes eyesOnApp) {
+        this.appDriver = appDriver;
+        this.appPage = appPage;
+        this.eyesOnApp = eyesOnApp;
+    }
+
+    @Step
+    public void performOperation(String input1, String operator1, String input2, String operator2) {
+        appPage.chooseNumber(input1);
+        log("Selected No." + input1);
+        appPage.chooseOperation(operator1);
+        log("Select Calculator Operation " + operator1);
+        appPage.chooseNumber(input2);
+        log("Select No." + input2);
+        captureAndAttachScreenshot(appDriver, "Data entered on Calculator");
+   //     eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
+        log("Select Calculator Operation " + operator2);
+        appPage.chooseOperation(operator2);
+    }
+
+    @Step
+    public void verifyResult(String expected) {
+        log("Expected output " + expected);
+        String actualOutput = appPage.getCalculatorDisplay();
+        log("Actual output " + actualOutput);
+        Assert.assertEquals(expected, actualOutput);
+        captureAndAttachScreenshot(appDriver, "Final result captured");
+   //     eyesOnApp.check(Thread.currentThread().getStackTrace()[1].getMethodName(), Target.window());
+    }
+
+
+    private static void log(String message) {
+        LOGGER.info(message);
+        logInfoMessage(message);
+    }
+}
