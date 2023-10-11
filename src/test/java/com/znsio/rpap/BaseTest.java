00c2edf6fb8672b45a06dd73c487201ff4a1e90d
Megha Pradeep <megha30pradeep@gmail.com>
2023-10-11 11:31:46 +0530

    moved driver setup to beforeMethod added setupApplitoolsInitializer method call to beforeSuite added afterMethod moved webdriverQuit to after method added method call closeBatch to afterSuite

--- src/test/java/com/znsio/rpap/BaseTest.java
+++ src/test/java/com/znsio/rpap/BaseTest.java
@@ -1,49 +1,71 @@
 package com.znsio.rpap;
 
 import com.znsio.applitools.integration.ApplitoolsInitializer;
+import com.znsio.rpap.pages.Page;
+import com.znsio.rpap.utils.DriverFactory;
 import io.appium.java_client.AppiumDriver;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import com.znsio.rpap.pages.Page;
-import com.znsio.rpap.utils.DriverFactory;
+import org.testng.xml.XmlTest;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 import static com.znsio.reportportal.integration.utils.ReportPortalLogger.logInfoMessage;
 import static com.znsio.rpap.utils.DriverFactory.WEB;
 
-public class BaseTest extends ApplitoolsInitializer {
-    protected static WebDriver driver;
+public class BaseTest {
+    protected WebDriver driver;
+    protected AppiumDriver appiumDriver;
     private static WebDriverWait wait;
     private static final Logger LOGGER = Logger.getLogger(BaseTest.class.getName());
     protected Page page;
+    protected ApplitoolsInitializer applitoolsInitializer;
 
     @BeforeSuite
-    public void suiteSetup() throws MalformedURLException {
+    public void suiteSetup(XmlTest suite) throws IOException {
         logInfoMessage("Inside @BeforeSuite of " + BaseTest.class.getSimpleName());
-        driver = DriverFactory.getDriver();
-        wait = DriverFactory.getWait(driver);
-        if (DriverFactory.getPlatform().equals(WEB)) {
-            ApplitoolsInitializer.driverSetupForApplitoolsInitializer(driver);
-        } else {
-            ApplitoolsInitializer.driverSetupForApplitoolsInitializer((AppiumDriver) driver);
-        }
-        logInfoMessage("Driver is ready");
+        applitoolsInitializer = new ApplitoolsInitializer();
+        applitoolsInitializer.setUpApplitoolsInitializer(suite);
     }
 
     @BeforeMethod
-    public void methodSetup() {
+    public void methodSetup(Method method) {
+        logInfoMessage("Inside @BeforeMethod of " + BaseTest.class.getSimpleName());
+        driver = DriverFactory.getDriver();
+        wait = DriverFactory.getWait(driver);
+        logInfoMessage("Driver is ready");
         page = new Page(driver, wait);
         logInfoMessage("Page setup is completed");
+        if (DriverFactory.getPlatform().equals(WEB)) {
+            applitoolsInitializer.driverSetupForApplitoolsInitializer(driver);
+        } else {
+            applitoolsInitializer.driverSetupForApplitoolsInitializer((AppiumDriver) driver);
+        }
+        applitoolsInitializer.initiateApplitoolsInitializer(method);
+    }
+
+    @AfterMethod
+    public void methodTearDown(ITestResult iTestResult, Method method) {
+        logInfoMessage("Inside @AfterMethod of " + BaseTest.class.getSimpleName());
+        applitoolsInitializer.closeApplitoolsInitializer(iTestResult);
+        DriverFactory.killDriver();
     }
 
     @AfterSuite
     public void suiteTearDown() {
-        logInfoMessage("Killing driver");
-        DriverFactory.killDriver();
+        logInfoMessage("Inside @AfterSuite of " + BaseTest.class.getSimpleName());
+        applitoolsInitializer.closeBatch();
+    }
+
+    public void log(String message) {
+        LOGGER.info(message);
+        logInfoMessage(message);
     }
 }
